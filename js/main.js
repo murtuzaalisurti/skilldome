7a33e5ebbf68efdfad2c3348e25f0a36c5c64926
murtuzaalisurti <68743212+murtuzaalisurti@users.noreply.github.com>
2020-12-05 19:18:22 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var btn1 = document.getElementById("cta-btn-1");
+console.log(btn1);
+btn1.addEventListener("click", function () {
+    document.getElementById("different-skills").scrollIntoView({ behavior: "smooth" });
+
+});
+
