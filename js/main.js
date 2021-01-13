0b4e40006acc4bb33f56ec2aac6c641b48fba0d4
Murtuza <68743212+murtuzaalisurti@users.noreply.github.com>
2021-01-13 11:56:49 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-var btn1 = document.getElementById("cta-btn-1");
-console.log(btn1);
-btn1.addEventListener("click", function () {
-    document.getElementById("different-skills").scrollIntoView({ behavior: "smooth" });
+document.addEventListener("DOMContentLoaded", function () {
+    var btn1 = document.querySelector("#cta-btn-1");
+    console.log(btn1);
+    btn1.addEventListener("click", function () {
+        document.getElementById("different-skills").scrollIntoView({ behavior: "smooth" });
 
+    });
 });
 
